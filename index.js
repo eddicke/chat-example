52a17e2e713c75977e84796aa2464890fe29b358
eddicke <adenekanademola931@gmail.com>
2018-05-26 23:47:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -96,8 +96,8 @@ io.on('connection', function(socket) {
     if(data.run){
      // dir.from = 450;
      // dir.to = 550;
-      dir.from = 700;
-      dir.to = 900;
+      dir.from = 1000;
+      dir.to = 2000;
     }
   })
   socket.on('getPosition', function(data) {
